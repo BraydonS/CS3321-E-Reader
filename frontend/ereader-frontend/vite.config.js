2c98baf5b6ce6655fd4aabd60b4af81f1dfb67fe
BraydonS <braydonspaulding1@gmail.com>
2022-12-02 10:57:06 -0700

    update config

--- frontend/ereader-frontend/vite.config.js
+++ frontend/ereader-frontend/vite.config.js
@@ -5,9 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({ 
-  build: {
-  assetsDir: 'CS3321-E-Reader/assets'
-  },
+  base: '/CS3321-E-Reader/',
   plugins: [vue()],
   resolve: {
     alias: {
