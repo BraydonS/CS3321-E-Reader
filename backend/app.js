46aa99ec91fed2197746b1634a3840f8917fe438
khs0723 <rlagustn19944@gmail.com>
2022-12-03 01:56:48 -0700

    pdf file upload and read

--- backend/app.js
+++ backend/app.js
@@ -5,11 +5,18 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
+const {MongoClient} = require('mongodb');
+
+//gridfs
+const multer = require('multer');
+const {GridFsStorage} = require('multer-gridfs-storage');
 
 const User = require("./models/user");
 
 const MONGODB_URI =
   "mongodb+srv://hyunsuKim:kim12345@cluster0.iixmbo0.mongodb.net/library?retryWrites=true&w=majority";
+
+const client = new MongoClient(MONGODB_URI);
 
 const app = express();
 const store = new MongoDBStore({
@@ -23,6 +30,7 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const authRoutes = require("./routes/auth");
 
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
@@ -46,6 +54,9 @@ app.use((req, res, next) => {
     .catch((err) => console.log(err));
 });
 
+
+
+
 app.use("/admin", adminRoutes);
 app.use(authRoutes);
 
@@ -57,3 +68,54 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+//gridfs creating bucket
+let bucket;
+
+mongoose.connection.on("connected", () => {
+  var db = mongoose.connections[0].db;
+  bucket = new mongoose.mongo.GridFSBucket(db, {
+    bucketName: "newBucket"
+  });
+  console.log(bucket);
+});
+
+const storage = new GridFsStorage({
+  url: MONGODB_URI,
+  file:(req, file) => {
+    return new Promise((resolve, reject) => {
+      const filename = file.originalname;
+      const fileInfo = {
+        filename : filename,
+        bucketName: "newBucket"
+      };
+      resolve(fileInfo)
+    })
+  }
+})
+
+const upload = multer({
+  storage
+});
+
+app.get("/fileinfo/:filename", (req, res) => {
+  const file = bucket
+    .find({
+      filename: req.params.filename
+    })
+    .toArray((err, files) => {
+      if (!files || files.length === 0) {
+        return res.status(404)
+          .json({
+            err: "no files exist"
+          });
+      }
+      bucket.openDownloadStreamByName(req.params.filename)
+        .pipe(res);
+    });
+});
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.status(200)
+    .send("File uploaded successfully");
+});
